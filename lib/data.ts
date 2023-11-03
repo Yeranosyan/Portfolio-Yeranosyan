03ae6227ee1c03250f1023dd577855099466cc69
Gago Yerevan <karlenovich@yahoo.com>
2023-11-02 20:56:24 -0700

    add description

--- lib/data.ts
+++ lib/data.ts
@@ -50,7 +50,7 @@ export const experiencesData = [
     title: "Johns Hopkins University Course",
     location: "Vancouver, BC",
     description:
-      "Before enrolling in formal courses, I completed courses on CodeCademy: HTML, CSS, JavaScript, React.js. Following that, I independently worked on my own small projects. Afterward, I enrolled in a course offered by Johns Hopkins University on the Coursera platform, where I gained a solid understanding of frontend development, including semantic structure and the Document Object Model (DOM). I extensively worked with HTML, CSS, JavaScript, and Bootstrap CSS framework. I also acquired proficiency in responsive design.",
+      "Prior to formal coursework, I proactively completed Codecademy courses in HTML5, CSS3, JavaScript, and React.js, establishing a strong foundation in web development. Following this, I independently undertook small-scale projects to apply and reinforce my skills. Subsequently, I furthered my education by enrolling in a Johns Hopkins University course, where I acquired a robust understanding of front-end development, with a focus on semantic structure and the Document Object Model (DOM). My hands-on experience included extensive work with HTML, CSS, JavaScript, and the Bootstrap CSS framework, enabling proficiency in responsive design techniques. This period of self-directed learning and structured education significantly contributed to my skill set and prepared me for more advanced coursework and real-world application.",
     icon: React.createElement(SiCoursera),
     date: "Aug 6, 2023",
   },
@@ -58,7 +58,7 @@ export const experiencesData = [
     title: "Meta Front-End Developer Professional Certificate",
     location: "Vancouver, BC",
     description:
-      "In the Front-End Meta course, I learned React and its main hooks: UseState, UseEffect, and Context API. I learned how to create a project, develop individual components, use npm, and successfully completed the final project on my GitHub. Additionally, a portion of the course was devoted to version control. I also acquired knowledge of the basics of UX/UI design and was introduced to Figma.",
+      "In the Front-End Meta course, I achieved proficiency in React, encompassing its key hooks, including UseState, UseEffect, useContext, and useReducer, as well as mastery of the Context API. This entailed practical experience in project development, component creation, and npm for effective package management. The successful delivery of the final project is available for review on my GitHub repository. The course also emphasized version control best practices. Furthermore, I acquired expertise in UX/UI design principles and now utilize Figma, enriching my skill set in front-end development.",
     icon: React.createElement(SiMeta),
     date: "Jul 31, 2023",
   },
@@ -66,9 +66,16 @@ export const experiencesData = [
     title: "Meta Back-End Developer Professional Certificate",
     location: "Vancouver, BC",
     description:
-      "In the Back-End Meta course, I learned Python for writing the back-end. I also worked with MySQL, connecting to the database, creating tables, and becoming familiar with types of relationships and how to use Primary Keys (PK) and Foreign Keys (FK) between tables. I worked with Django, a web framework for building robust and user-friendly web applications. Additionally, I gained knowledge in RESTful API, API endpoints, and testing APIs using Postman and Insomnia. I also learned about serializers and deserializers, building a data model, and utilizing version control.",
+      "In the Back-End Meta course, I honed my skills in Python for back-end development. I mastered MySQL for database management, covering table relationships, Primary Keys (PK), and Foreign Keys (FK). With Django, I built robust web applications, and I gained expertise in RESTful APIs, API endpoints, and their testing using Postman and Insomnia. Additionally, I learned about serializers, deserializers, data modeling, and effective version control.",
     icon: React.createElement(SiMeta),
     date: "Sep 12, 2023",
+  },
+  {
+    title: "Meta Full-Stack Engineer Certificate",
+    location: "Vancouver, BC",
+    description:
+      "I am a skilled React engineer with a strong background in front-end development. My expertise includes crafting dynamic front-end interfaces with React and JSX. In addition, I am proficient in Python and Django, enabling seamless integration between the front-end and back-end. My experience extends to testing API endpoints, implementing Redux for effective state management, and working with both functional and stateful components. My commitment to continuous learning ensures that I stay at the forefront of web development technologies.",
+    date: "Oct 10, 2023",
   },
   {
     title: "IBM Full-Stack Software Developer Professional Certificate",
