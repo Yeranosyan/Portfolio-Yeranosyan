4f3986127752a396177e8e43625a0db922184566
Gago Yerevan <karlenovich@yahoo.com>
2023-12-29 21:52:35 -0800

    add features

--- components/intro.tsx
+++ components/intro.tsx
@@ -100,7 +100,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none border border-black/10 focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer dark:bg-white/10"
-          href="/Full-Stack_Gagik_Yeranosyan.pdf"
+          href="/Gagik-Full-Stack-Developer.pdf"
           download
         >
           Download Resume{" "}
