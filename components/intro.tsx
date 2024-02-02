20f47470c4a988455cba08ef27ae21864a3e4f11
Gago Yerevan <karlenovich@yahoo.com>
2024-02-01 18:59:31 -0800

    chahges small

--- components/intro.tsx
+++ components/intro.tsx
@@ -78,7 +78,7 @@ export default function Intro() {
           development.
         </span>
         <br />
-        <span className="text-sm italic text-gray-500 dark:text-pink-200">
+        <span className="text-sm italic text-gray-500 dark:text-lime-200">
           cost efficient and result oriented
         </span>
       </motion.h1>
@@ -105,7 +105,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none border border-black/10 focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer dark:bg-white/10"
-          href="/Gagik-Full-Stack-Developer.pdf"
+          href="/Gagik-Yeranosyan_Software-Developer.pdf"
           download
         >
           Download Resume{" "}
