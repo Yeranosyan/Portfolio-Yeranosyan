87fdccfe207b79cbd816e498e72e689cec96a5bc
Yeranosyan <karlenovich@yahoo.com>
2024-02-29 21:29:45 -0800

    fix color

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,8 +37,8 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36  dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#404e65]"></div>
-        <div className="bg-[#d7dbfb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:-[-5rem] dark:bg-[#3a4058]"></div>
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#3c6b9b]"></div>
+        <div className="bg-[#d7dbfb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:-[-5rem] dark:bg-[#2c4269]"></div>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
