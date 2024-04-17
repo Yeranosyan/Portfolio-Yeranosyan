d93515f15628cf98b62c8cb2e9566759e687d962
Yeranosyan <karlenovich@yahoo.com>
2024-04-17 11:02:45 -0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,6 @@ export const metadata = {
   author: "Gagik Yeranosyan",
 };
 
-// In ReactJS with TypeScript, ReactNode is a type that represents a React element, an array of React elements, or a string, number, or boolean.
 export default function RootLayout({
   children,
 }: {
@@ -37,8 +36,8 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36  dark:bg-black/90 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#2b2b2b]"></div>
-        <div className="bg-[#d7dbfb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:-[-5rem] dark:bg-[#50505042]"></div>
+        {/* <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#2b2b2b]"></div>
+        <div className="bg-[#d7dbfb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:-[-5rem] dark:bg-[#50505042]"></div> */}
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
