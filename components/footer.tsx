7afc26faf0857e42d6e081009f38af044432d665
Yeranosyan <karlenovich@yahoo.com>
2025-04-23 09:18:59 -0700

    lounge update

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className=" max-w-3xl mb-10 px-4 text-center text-gray-400">
       <small className="mb-2 block text-xs">
-        &copy; 2023 Gagik Yeranosyan. All rights reserved.
+        &copy; 2025 Gagik Yeranosyan. All rights reserved.
       </small>
       <span className="font-semibold">About this website:</span> build whit
       React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
